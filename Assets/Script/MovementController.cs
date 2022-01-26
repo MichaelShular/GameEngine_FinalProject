d734317d4b7e5ba070814f26d8f135b7b4863895
Mick <mickx.shular@gmail.com>
2022-01-25 21:31:15 -0500

    running blend tree and imported new jumping animation

--- Assets/Script/MovementController.cs
+++ Assets/Script/MovementController.cs
@@ -14,12 +14,20 @@ public class MovementController : MonoBehaviour
     
     PlayerController playerController;
     Rigidbody playerRigidbody;
+    Animator playerAnimator;
 
     Vector2 inputVector = Vector2.zero;
     Vector3 moveDirection = Vector3.zero;
 
+    public readonly int movementXHash = Animator.StringToHash("MovementX");
+    public readonly int movementYHash = Animator.StringToHash("MovementY");
+    public readonly int isJumpingHash = Animator.StringToHash("isJumping");
+    public readonly int isRunningHash = Animator.StringToHash("isRunning");
+
+
     private void Awake()
     {
+        playerAnimator = GetComponent<Animator>();
         playerController = GetComponent<PlayerController>();
         playerRigidbody = GetComponent<Rigidbody>();
     }
@@ -54,11 +62,14 @@ public class MovementController : MonoBehaviour
     public void OnMovement(InputValue value)
     {
         inputVector = value.Get<Vector2>();
+        playerAnimator.SetFloat(movementXHash, inputVector.x);
+        playerAnimator.SetFloat(movementYHash, inputVector.y);
     }
 
     public void OnRun(InputValue value)
     {
         playerController.isRunning = value.isPressed;
+        playerAnimator.SetBool(isRunningHash, playerController.isRunning);
     }
 
     public void OnJump(InputValue value)
@@ -68,7 +79,7 @@ public class MovementController : MonoBehaviour
         playerController.isJumping = true;
  
         playerRigidbody.AddForce((transform.up + moveDirection) * jumpForce, ForceMode.Impulse);
-        
+        playerAnimator.SetBool(isJumpingHash, playerController.isJumping);
     }
 
     private void OnCollisionEnter(Collision collision)
